8e618da3fe173f36cc918518058dee71f76b9d95
Mahima Rohila <114685825+mahimarohila@users.noreply.github.com>
2024-02-18 21:06:42 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://mahimarohila600:Mahima1234@ieee.73vilvm.mongodb.net/?retryWrites=true&w=majority",{
+mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser:true, 
     useUnifiedTopology: true,
 }).then(()=> {
