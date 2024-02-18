8e6cdbcde5eb84c0b144ceda5e89fa2fb517f2f6
Mahima Rohila <114685825+mahimarohila@users.noreply.github.com>
2024-02-18 20:08:06 +0530

    Update contacts.js

--- src/models/contacts.js
+++ src/models/contacts.js
@@ -18,7 +18,7 @@ const employeeSchema = new mongoose.Schema({
         type:String
     }
 })
-employeeSchema.index({ email: 1, comment: 1 }, { unique: true, partialFilterExpression: { comment: { $exists: true } } });
+
 
 
 const Contact = new mongoose.model("Contact", employeeSchema);
